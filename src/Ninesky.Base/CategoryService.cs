b6a126929023f741db706aa74e2d0aea147b30d3
洞庭夕照 <mzwhj@live.cn>
2016-12-06 00:41:49 +0800

    --

---
+++ src/Ninesky.Base/CategoryService.cs
@@ -0,0 +1,39 @@
+﻿/*======================================
+ 作者：洞庭夕照
+ 创建：2016.12.05
+ 网站：www.ninesky.cn
+       mzwhj.cnblogs.com
+ 代码：git.oschina.net/ninesky/Ninesky
+ 版本：v1.0.0.0
+ =====================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Ninesky.DataLibrary;
+
+namespace Ninesky.Base
+{
+    /// <summary>
+    /// 栏目服务类
+    /// </summary>
+    public class CategoryService
+    {
+        private BaseRepository<Category> _baseRepository;
+        public CategoryService(DbContext dbContext)
+        {
+            _baseRepository = new BaseRepository<Category>(dbContext);
+        }
+
+        /// <summary>
+        /// 查找
+        /// </summary>
+        /// <param name="Id">栏目Id</param>
+        /// <returns></returns>
+        public Category Find(int Id)
+        {
+            return _baseRepository.Find(Id);
+        }
+    }
+}
---
+++ src/Ninesky.DataLibrary/BaseRepository.cs
@@ -0,0 +1,38 @@
+﻿/*======================================
+ 作者：洞庭夕照
+ 创建：2016.12.05
+ 网站：www.ninesky.cn
+       mzwhj.cnblogs.com
+ 代码：git.oschina.net/ninesky/Ninesky
+ 版本：v1.0.0.0
+ =====================================*/
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Ninesky.DataLibrary
+{
+    /// <summary>
+    /// 仓储基类
+    /// </summary>
+    public class BaseRepository<T> where T :class
+    {
+        private DbContext _dbContext;
+        public BaseRepository(DbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        /// <summary>
+        /// 查询
+        /// </summary>
+        /// <param name="Id">主键</param>
+        /// <returns>实体</returns>
+        public T Find(int Id)
+        {
+            return _dbContext.Set<T>().Find(Id);
+        }
+    }
+}
